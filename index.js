fa388dfca3d2efa8128c6caf54e82738b30631e5
Donald <donald_langston@yahoo.com>
2020-03-08 22:55:57 -0400

    Part 2

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-    function renderMovies(movieArray) {
+    
+    function renderMovies(movieArray) {    
        var movieHTML = movieArray.map(function(currentMovie) {
+
             return `<div class="movie card" style="width: 18rem;">
             <img src="${currentMovie.Poster}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${currentMovie.Title}</h5>
               <p class="card-text">${currentMovie.Year}</p>
-              <a href="#" class="btn btn-primary">Add</a>
+              <a href="#" class="btn btn-primary" onclick="saveToWatchlist('${currentMovie.imdbID}')">Add</a>
             </div>
         </div>`
         });
-        return movieHTML.join("");
+        return movieHTML.join("")
     }
+
+    
     document.getElementById('search-form').addEventListener('submit', function(e) {
         e.preventDefault();
         document.getElementsByClassName("movies-container")[0].innerHTML = renderMovies(movieData);
     });
+ 
 });
 
 
+function saveToWatchlist(imdbID) {
+    var movie = movieData.find(function(currentMovie) {
+        return currentMovie.imdbID == imdbID;
+    });
+    
+    console.log(movie);
+    var watchlistJSON = localStorage.getItem('watchlist');
+    var watchlist = JSON.parse(watchlistJSON);
+
+    if(watchlist == null) {
+        watchlist = [];
+    }
+    watchlist.push(movie);
+    watchlistJSON = JSON.stringify(watchlist);
+    localStorage.setItem('watchlist', watchlistJSON);
+}
 
---
+++ watchlist.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var watchlistJSON = localStorage.getItem('watchlist');
+    var watchList = JSON.parse(watchlistJSON);
+     var watchListHTML = watchList.map(function(currentMovie) {
+        return `<div class="movie card col-4" style="width: 18rem;">
+        <img src="${currentMovie.Poster}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${currentMovie.Title}</h5>
+          <p class="card-text">${currentMovie.Year}</p>
+          <a href="#" class="btn btn-primary" onclick="saveToWatchlist('${currentMovie.imdbID}')">Add</a>
+        </div>
+    </div>` 
+    });
+    watchListHTML.join("");
+    document.getElementsByClassName("movies-container")[0].innerHTML = watchListHTML;
+});
